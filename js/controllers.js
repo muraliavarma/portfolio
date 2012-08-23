cbc5dbc0b4c235e31ddd8428b4d4e574ee9efc43
Murali <muraliavarma@gmail.com>
2012-08-23 02:43:32 -0400

    First commit

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+function ProjectCtrl($scope) {
+	$scope.projects = [
+		{
+			id: 'gpu',
+			name: 'Rendering Pipeline in JavaScript'
+		},
+		{
+			id: 'pool',
+			name: 'OpenGL Rigid Body Pool Gamesh'
+		}
+	];
+}
