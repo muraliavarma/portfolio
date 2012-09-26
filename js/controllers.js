a0686a9483edf9068e14dfad1eb8ed55ea927b3c
Murali <muraliavarma@gmail.com>
2012-09-26 02:26:50 -0400

    Set up framework. Added data for JavaScript projects

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,26 @@
-function ProjectCtrl($scope) {
-	$scope.categories = [{
-		language: 'JavaScript',
-		projects:[{
-			name: 'Rendering Pipeline in JavaScript',
-			featured: true
-		},{
-			name: 'Portfolio in AngularJS'
-		},{
-			name: 'Smoothed Particle Hydrodynamics'
-		}]
-	},{
-		language: 'Unity',
-		projects:[{
-			name: 'Airplane Shooter',
-			featured: true
-		},{
-			name: 'Breakout Game'
-		},{
-			name: 'Fluffy - 2D Platformer'
-		},{
-			name: 'Multi Tag Manager'
-		}]
-	}];
+function ProjectCtrl($scope, $http) {
+	$http.get('data/categories.json').success(function(data) {
+		$scope.categories = data;
+	});
+
+
+	$scope.setDetails = function(project) {
+		$scope.setSelected(project.id);
+		$http.get('data/' + project.id + '.json').success(function(data) {
+			$scope.details = data;
+			$scope.details.title = project.name;
+			$scope.details.featured = project.featured;
+		}).error(function(data, status, headers, config) {
+			$scope.details = {};
+		});
+	};
+
+	$scope.setSelected = function(projectId) {
+		for (var i = 0; i < $scope.categories.length; i++) {
+			for (var j = 0; j < $scope.categories[i].projects.length; j++) {
+				$scope.categories[i].projects[j].selected = ($scope.categories[i].projects[j].id == projectId);
+			}
+		}
+	}
+
 }
